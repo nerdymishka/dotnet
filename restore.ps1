4fd3e92ef5a7d610d515c3148e56622a23b7e6cd
Michael Herndon <mherndon@nerdymishka.com>
2020-01-21 14:41:25 -0600

    update restore.ps1
    - fix restore code to work

--- restore.ps1
+++ restore.ps1
@@ -2,7 +2,7 @@ Param(
     [Switch] $Force
 )
 
-if([Environment]::OSVersion.Platform -ne "Win32NT") {
+if ([Environment]::OSVersion.Platform -ne "Win32NT") {
     # TODO: add a restore.debian.ps1
     throw "restore.ps1 only works on windows"
 }
@@ -12,24 +12,67 @@ function Update-Environment() {
     Set-Alias "ptest" "invoke-pester" -Scope Global 
 }
 
-if((Test-Path "./.setup") -and !$Force.ToBool()) { 
+if ((Test-Path "./.setup") -and !$Force.ToBool()) { 
     Update-Environment
     exit 
 }
+$mods = Get-Module -ListAvailable
 
-Install-Module Gz-ChocolateySync -AllowClobber -Force -SkipPublisherCheck
-Install-Module Psake -SkipPublisherCheck -AllowClobber -Force
-Install-Module Pester -SkipPublisherCheck -Force -AcceptLicense -AllowClobber
+function Install-Mod {
+    Param(
+        [String] $Name,
+        [hashtable] $ArgumentList 
+    )
 
-if($null -eq (Get-Command choco -EA SilentlyContinue))
-{
-    if(!$Force.ToBool())
-    {
+    $args = @{
+        "SkipPublisherCheck" = $true 
+        "AllowClobber"       = $true 
+        "Force"              = $true 
+    }
+    if ($ArgumentList) {
+        foreach ($item in $ArgumentList.Keys) {
+            $args[$item] = $ArgumentList[$item]
+        }
+    }
+
+    if ($mods.Name.Contains($Name)) {
+   
+        if (!$args["RequiredVersion"]) {
+            return;
+        }
+
+        $version = New-Object System.Version -ArgumentList $args["RequiredVersion"]
+        $mod = $mods | Where-Object { $_.Name -eq $Name } | Select-Object -First 
+        
+        if ($mod -and $version -lt $mod.Version.ToString()) {
+            Update-Module $Name -RequiredVersion $args["RequiredVersion"]
+            return 
+        }
+        else {
+            return 
+        }
+    }
+
+    $scope = "AllUsers" 
+    $isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+    if (!$isAdmin) {
+        $scope = "CurrentUser"
+    }
+
+    Install-Module $Name @args -Scope $scope 
+    return 
+}
+
+Install-Mod Gz-ChocolateySync 
+Install-Mod Psake
+Install-Mod Pester 
+
+if ($null -eq (Get-Command choco -EA SilentlyContinue)) {
+    if (!$Force.ToBool()) {
         Write-Warning "Chocolatey is required and not installed on path"
         Write-Warning "If you continue, chocolatey will be installed to c:/apps"
         $stop = Read-Host  "install chocolatey now? Press 'y' to continue"
-        if($stop -ne "y")
-        {
+        if ($stop -ne "y") {
             exit 
         }
     }   
@@ -37,12 +80,12 @@ if($null -eq (Get-Command choco -EA SilentlyContinue))
 
 Sync-Chocolatey -Uri "$PsSCriptRoot/build/chocolatey.json"
 
-if($null -eq (Get-Command dotnet -EA SilentlyContinue)) {
+if ($null -eq (Get-Command dotnet -EA SilentlyContinue)) {
     $env:Path += ";$Env:ProgramFiles\dotnet"
 }
 
 # not available via chocolatey
-if($null -eq (Get-Command nbgv -EA SilentlyContinue)) {
+if ($null -eq (Get-Command nbgv -EA SilentlyContinue)) {
     dotnet tool install -g nbgv 
 }
 
@@ -51,24 +94,24 @@ $gitRemotes = git remote
 $remotes = @{
     "gitlab" = "https://gitlab.com/nerdymishka/dotnet"
     "github" = "https://github.com/nerdymishka/dotnet"
-    "vsts" = "https://dev.azure.com/nerdymishka/dotnet/_git/dotnet"
+    "vsts"   = "https://dev.azure.com/nerdymishka/dotnet/_git/dotnet"
 }
 
-foreach($key in $remotes.Keys) {
+foreach ($key in $remotes.Keys) {
     
-    if($gitRemotes.Contains($key)) {
+    if ($gitRemotes.Contains($key)) {
         continue;
     }
     
-    if(!$gitRemotes.Contains($key)) {
-        git remote add $key "$($remote[$key])"
+    if (!$gitRemotes.Contains($key)) {
+        git remote add $key "$($remotes[$key])"
     }
 
-    if($key -eq "origin") {
+    if ($key -eq "origin") {
         continue 
     }
 
-    git remote set-url origin --push --add "$($remote[$key])"
+    git remote set-url origin --push --add "$($remotes[$key])"
 }
 
 Update-Environment
